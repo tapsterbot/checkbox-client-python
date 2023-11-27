7fdd998fe3218b72885f9e63370f7d0de6fcf737
Jason Huggins <jrhuggins@gmail.com>
2023-11-27 06:36:22 -0600

    Initial commit of code. 🎉

---
+++ checkbox.py
@@ -0,0 +1,313 @@
+import json
+import requests
+from requests.compat import urljoin
+
+# Default host:
+HOST = "http://checkboxmini.local:5000"
+
+class Ping:
+    def __init__(self, host = HOST):
+        self._host = host
+
+    def ping(self, host = HOST):
+        """
+        Ping server
+
+        Command line equivalent:
+        $ curl $HOST/api/ping
+        """
+        url = urljoin(self._host, "/api/ping")
+        print(url)
+        response = requests.get(url)
+
+        return response.json()
+
+
+class Config:
+    def __init__(self, host = HOST):
+        self._host = host
+
+    def mouse_position(self):
+        """
+        Get mouse position
+
+        Command line equivalent:
+        $ curl $HOST/api/config/mouse/position
+        """
+        url = urljoin(self._host, "/api/config/mouse/position")
+        response = requests.get(url)
+
+        return response.json()
+
+    def mouse_screen_position(self):
+        """
+        Get mouse position as screen coordinates
+
+        Command line equivalent:
+        $ curl $HOST/api/config/mouse/screen-position
+        """
+        url = urljoin(self._host, "/api/config/mouse/screen-position")
+        response = requests.get(url)
+
+        return response.json()
+
+class Keyboard:
+    def __init__(self, host = HOST):
+        self._host = host
+
+    def type(self, text = ""):
+        """
+        Type text
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/keyboard/type \
+            -H "Content-Type: application/json" \
+            -d '{"text": "yo"}'
+        """
+        url = urljoin(self._host, "/api/keyboard/type")
+        payload = {'text': text}
+        response = requests.post(url, json = payload)
+
+
+class RawMouse:
+    def __init__(self, host = HOST):
+        self._host = host
+
+    def move_by(self, x = 0, y = 0):
+        """
+        Move x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/raw/mouse/move/by \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/raw/mouse/move/by")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+    def drag(self, x = 0, y = 0):
+        """
+        Drag x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/raw/mouse/drag/by \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/raw/mouse/drag/by")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+class Mouse:
+    """
+    Example Usage:
+    >>> from checkbox import Mouse
+    >>> m = Mouse()
+    >>> m.home()
+    """
+    def __init__(self, host = HOST):
+        self._host = host
+        self.x = 0
+        self.y = 0
+        self.calibrated = False
+        self.raw = RawMouse()
+
+    def jiggle(self):
+        """
+        Jiggle the mouse
+
+        Command line equivalent:
+        $ curl $HOST/api/mouse/jiggle
+        """
+        url = urljoin(self._host, "/api/mouse/jiggle")
+        response = requests.get(url)
+
+    def down(self):
+        """
+        Send a mouseDown event
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse/down \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse/down")
+        response = requests.post(url)
+
+    def up(self):
+        """
+        Send a mouseUp event
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse/up \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse/up")
+        response = requests.post(url)
+
+    def click(self):
+        """
+        Shortcut for down(), up()
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse/click -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse/click")
+        response = requests.post(url)
+
+    def move_by(self, x = 0, y = 0):
+        """
+        Move x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse/move/by \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/mouse/move/by")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+    def home(self):
+        """
+        Move mouse home
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse/move/home -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse/move/home")
+        response = requests.post(url)
+        #self.x = 40
+        #self.y = 40
+        # Mouse position should now logically be (x=40, y=40)
+
+class RawMouseKeys:
+    def __init__(self, host = HOST):
+        self._host = host
+
+    def move_by(self, x = 0, y = 0):
+        """
+        Move x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/raw/mouse-keys/move/by \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/raw/mouse-keys/move/by")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+    def drag(self, x = 0, y = 0):
+        """
+        Drag x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/raw/mouse-keys/drag/by \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/raw/mouse-keys/drag/by")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+class MouseKeys:
+    """
+    Example Usage:
+    >>> from checkbox import MouseKeys
+    >>> mk = MouseKeys()
+    >>> mk.home()
+    """
+    def __init__(self, host = HOST):
+        self._host = host
+        self.x = 0
+        self.y = 0
+        self.calibrated = False
+        self.raw = RawMouseKeys()
+
+    def jiggle(self):
+        """
+        Jiggle the mouse
+
+        Command line equivalent:
+        $ curl $HOST/api/mouse/jiggle
+        """
+        url = urljoin(self._host, "/api/mouse-keys/jiggle")
+        response = requests.get(url)
+
+    def down(self):
+        """
+        Send a mouseDown event
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse-keys/down \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse-keys/down")
+        response = requests.post(url)
+
+    def up(self):
+        """
+        Send a mouseUp event
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse-keys/up \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse-keys/up")
+        response = requests.post(url)
+
+    def click(self):
+        """
+        Send a mouse click
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse-keys/click \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse-keys/click")
+        response = requests.post(url)
+
+    def move_to(self, x = 0, y = 0):
+        """
+        Move x, y pixels
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse-keys/move/to \
+            -H "Content-Type: application/json" \
+            -d '{"x": 0, "y": 0}'
+        """
+        url = urljoin(self._host, "/api/mouse-keys/move/to")
+        payload = {'x': x, 'y': y}
+        response = requests.post(url, json = payload)
+
+    def home(self):
+        """
+        Move mouse home
+
+        Command line equivalent:
+        $ curl -X POST $HOST/api/mouse-keys/move/home \
+            -H "Content-Type: application/json"
+        """
+        url = urljoin(self._host, "/api/mouse-keys/move/home")
+        response = requests.post(url)
+        self.x = 40
+        self.y = 40
+        # Mouse position should now logically be (x=40, y=40)
+
+class Screenshot:
+    def __init__(self, host = HOST):
+        self._host = host    
+    
+    def get_screenshot(self, filename = "screenshot.jpeg"):
+        """
+        Type text
+
+        Command line equivalent:
+        $ curl $HOST/api/screenshot -o screenshot.jpeg
+        """
+        url = urljoin(self._host, "/api/screenshot")
+        response = requests.get(url)
+        file = open(filename, "wb")
+        file.write(response.content)
+        file.close()
---
+++ test.py
@@ -0,0 +1,28 @@
+# To run script and start an interactive session (REPL):
+# $ python -i test.py --host=http://checkboxmini.local:5000
+
+from checkbox import Ping, Config, Keyboard, Mouse, MouseKeys, Screenshot
+
+import argparse
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--host', help="host of the Checkbox API server\n" + \
+                                    "default host is http://checkboxmini.local:5000\n" + \
+                                    "example:\n  python test.py --host=http://example.local:5000")
+args = parser.parse_args()
+if args.host:
+    host = args.host
+else:
+    host = "http://checkboxmini.local:5000"
+
+print("Using API host: %s" % host)
+
+# Class init
+c = Config(host)
+k = Keyboard(host)
+p = Ping(host)
+m = Mouse(host)
+mk = MouseKeys(host)
+s = Screenshot(host)
+
+# Shortcuts
+ping = p.ping
